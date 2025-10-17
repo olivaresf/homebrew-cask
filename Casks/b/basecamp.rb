106b0a87cd85f724d61c7644f034106f7af92732
Fernando <fernando.olivares@me.com>
2025-10-17 10:27:23 -0600

    Update basecamp.rb

--- Casks/b/basecamp.rb
+++ Casks/b/basecamp.rb
@@ -5,14 +5,14 @@ cask "basecamp" do
   sha256 arm:   "21eb55ba6f08e97fdc2400dad2df9d3d2cd360e2580d679b766611b295ef1293",
          intel: "0779bbfe9ce79e6655c35256e079541eb19a732b4099d9628854442e72d4bb7b"
 
-  url "https://bc#{version.major}-desktop.s3.amazonaws.com/mac#{arch}/basecamp#{version.major}-#{version.csv.second}.zip",
-      verified: "bc3-desktop.s3.amazonaws.com/"
+  url "https://basecamp.com/desktop/mac#{arch}/basecamp#{version.major}-#{version.csv.second}.zip",
+      verified: "basecamp.com/"
   name "Basecamp"
   desc "All-In-One Toolkit for Working Remotely"
   homepage "https://basecamp.com/help/#{version}/guides/apps/mac"
 
   livecheck do
-    url "https://bc#{version.major}-desktop.s3.amazonaws.com/mac#{arch}/updates.json"
+    url "https://basecamp.com/desktop/mac#{arch}/updates.json"
     regex(/basecamp(\d*)[_-]v?(\d+(?:\.\d+)+)/i)
     strategy :json do |json, regex|
       match = json["url"]&.match(regex)
